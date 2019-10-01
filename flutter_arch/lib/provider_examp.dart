4f2ee769de05967342ebfb8f15686c10fd1210c9
Bartosz Kosarzycki <bartosz.kosarzycki@intive.com>
2019-10-01 13:08:27 +0200

    simple components

--- flutter_arch/lib/provider_examp.dart
+++ flutter_arch/lib/provider_examp.dart
@@ -62,7 +62,18 @@ class LoginWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("LoginWidget");
+    return Padding(
+      padding: const EdgeInsets.only(left: 30.0, right: 30),
+      child: Column(
+        children: <Widget>[
+          FormContainer(),
+          Padding(
+            padding: const EdgeInsets.only(top: 25.0),
+            child: SignInButton(),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -79,5 +90,98 @@ class ProgressWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text("ProgressWidget");
+  }
+}
+
+// source: https://flutterawesome.com/a-new-open-source-flutter-login-home-animation/
+class FormContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (new Container(
+      margin: new EdgeInsets.symmetric(horizontal: 20.0),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Form(
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  new InputFieldArea(
+                    hint: "Username",
+                    obscure: false,
+                    icon: Icons.person_outline,
+                  ),
+                  new InputFieldArea(
+                    hint: "Password",
+                    obscure: true,
+                    icon: Icons.lock_outline,
+                  ),
+                ],
+              )),
+        ],
+      ),
+    ));
+  }
+}
+
+class InputFieldArea extends StatelessWidget {
+  final String hint;
+  final bool obscure;
+  final IconData icon;
+  InputFieldArea({this.hint, this.obscure, this.icon});
+  @override
+  Widget build(BuildContext context) {
+    return (new Container(
+      decoration: new BoxDecoration(
+        border: new Border(
+          bottom: new BorderSide(
+            width: 0.5,
+            color: Colors.deepPurpleAccent,
+          ),
+        ),
+      ),
+      child: new TextFormField(
+        obscureText: obscure,
+        style: const TextStyle(
+          color: Colors.purple,
+        ),
+        decoration: new InputDecoration(
+          icon: new Icon(
+            icon,
+            color: Colors.purple,
+          ),
+          border: InputBorder.none,
+          hintText: hint,
+          hintStyle: const TextStyle(color: Colors.purple, fontSize: 15.0),
+          contentPadding: const EdgeInsets.only(
+              top: 30.0, right: 30.0, bottom: 30.0, left: 5.0),
+        ),
+      ),
+    ));
+  }
+}
+
+class SignInButton extends StatelessWidget {
+  SignInButton();
+  @override
+  Widget build(BuildContext context) {
+    return (new Container(
+      width: 320.0,
+      height: 60.0,
+      alignment: FractionalOffset.center,
+      decoration: new BoxDecoration(
+        color: const Color.fromRGBO(247, 64, 106, 1.0),
+        borderRadius: new BorderRadius.all(const Radius.circular(30.0)),
+      ),
+      child: new Text(
+        "Sign In",
+        style: new TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      ),
+    ));
   }
 }
