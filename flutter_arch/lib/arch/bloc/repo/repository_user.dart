398942ef546d174a58aa5e43ff678ad75f2e5e57
Bartosz Kosarzycki <bartosz.kosarzycki@intive.com>
2019-12-07 19:01:44 +0100

    delay execution of login for bloc

--- flutter_arch/lib/arch/bloc/repo/repository_user.dart
+++ flutter_arch/lib/arch/bloc/repo/repository_user.dart
@@ -1,16 +1,22 @@
-
-import 'dart:io';
 import 'package:flutter_arch/arch/bloc/model/userlogin.dart';
 import 'package:flutter_arch/arch/bloc/model/usersession.dart';
 
 class UserRepository {
-
   @override
-  Future<UserSession> signIn(UserLogin userCredentials) {
+  Future<UserAuthenticationState> signIn(UserLogin userCredentials) async {
     // should call userService.signIn(userCredentials);
     print("Logging in...");
-    sleep(const Duration(seconds:1));
-    return Future.value(UserSession(userCredentials.login, "12876sad87x786c6"));
+    return Future.delayed(Duration(seconds: 1), () {
+      if (userCredentials.login.isNotEmpty &&
+          userCredentials.password.isNotEmpty) {
+        return UserSession(userCredentials.login, "12876sad87x786c6");
+      } else {
+        return UserUnauthenticated(
+          user: userCredentials.login,
+          exception: Exception("Wrong password"),
+        );
+      }
+    });
   }
 
   @override
