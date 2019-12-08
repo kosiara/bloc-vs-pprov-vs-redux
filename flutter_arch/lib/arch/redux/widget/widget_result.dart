5f61b8a737e676095074e81fbadc37789a49b6ef
Bartosz Kosarzycki <bartosz.kosarzycki@intive.com>
2019-12-08 20:00:29 +0100

    redux: check password correctness

--- flutter_arch/lib/arch/redux/widget/widget_result.dart
+++ flutter_arch/lib/arch/redux/widget/widget_result.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_arch/arch/redux/model/app_state.dart';
+import 'package:flutter_arch/arch/redux/model/userlogin.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class ResultWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final resultWidget = true ? Text("Result: " + (true ? "OK" : "WRONG"))
+
+    UserLogin user = StoreProvider.of<AppState>(context).state.loggedUser;
+    bool loggedIn = user != null;
+    bool correctPassword = loggedIn && user.checkCredentials();
+    final resultWidget = loggedIn ? Text("Result: " + (correctPassword ? "OK" : "WRONG"))
         : Text("");
     return Padding(
       padding: const EdgeInsets.only(top: 12.0),
